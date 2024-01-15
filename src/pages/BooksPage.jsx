558bbd27f259cdc553ce281c6bbe312a595422f8
SalvatoreBrancato <salvatorebrancato90@gmail.com>
2024-01-15 17:40:44 +0100

    homepage

--- src/pages/BooksPage.jsx
+++ src/pages/BooksPage.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 export default function BooksPage(){
     const [dati, setDati] = useState([])
 
-    function apiBooks(){
+    function apiBookResearch(){
       axios.get("https://www.googleapis.com/books/v1/volumes?q=intitle:l'ombra+del+vento")
         .then(response => {
           // Analizza la risposta
@@ -18,17 +18,24 @@ export default function BooksPage(){
         });
     }
     
-    //solo al primo rendering
-    useEffect(apiBooks, []);
   
     return (
       <>
-        {dati.map((book)=>{
-          return(
-            <h1>{book.volumeInfo.title}</h1>
-          )
-          
-        })}
+        <div className="w-full h-full flex justify-center bg-gradient-to-b from-slate-800  to-slate-600">
+          <form className='flex items-center h-1/3'>
+            <div className='m-5'>
+              <label htmlFor="book" className='text-white'>Testo: </label>
+              <input name="book" type="text" />
+            </div>
+            <div className='m-5'>
+              <label htmlFor="author" className='text-white'>Autore: </label>
+              <input name="author" type="text" />
+            </div>
+            <div>
+              <button className="p-1 bg-red-500 rounded-md m-5 text-white" type="submit">Ricerca</button>
+            </div>
+          </form>
+        </div>
       </>
     )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,9 +4,11 @@ import axios from 'axios';
 export default function HomePage() {
 
     const [dati, setDati] = useState([])
+    const [datiDue, setDatiDue] = useState([])
 
+    // chiamata api I sezione
     function apiBooks(){
-      axios.get("https://www.googleapis.com/books/v1/volumes?q=intitle:l'ombra+del+vento")
+      axios.get("https://www.googleapis.com/books/v1/volumes?q=inauthor:stephen+king&orderBy=newest&maxResults=30")
         .then(response => {
           // Analizza la risposta
           setDati(response.data.items);
@@ -17,21 +19,53 @@ export default function HomePage() {
           console.log(error);
         });
     }
+
+    //chiamata api II sezione
+    function apiBooksDue(){
+      axios.get("https://www.googleapis.com/books/v1/volumes?q=inauthor:glenn+cooper&orderBy=newest&maxResults=30")
+        .then(response => {
+          // Analizza la risposta
+          setDatiDue(response.data.items);
+           console.log(response.data.items)
+        })
+        .catch(error => {
+          // Gestisci gli errori
+          console.log(error);
+        });
+    }
     
     //solo al primo rendering
     useEffect(apiBooks, []);
+    useEffect(apiBooksDue, []);
 
     return (
       <>
-      <div className="w-screen  bg-gradient-to-b from-slate-800  to-slate-600 h-full">
-        <div className='h-full flex justify-around items-end snap-x'>
-          {/* <div>{dati.items.volume}</div> */}
+      <div className="w-full h-full flex flex-col justify-end  bg-gradient-to-b from-slate-800  to-slate-600">
+        {/* I sezione */}
+        <div className='flex overflow-x-auto flex-nowrap'>
           {dati.map((book)=>{
-            return(
-              <div key={book.id}>
-                <img src={book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.smallThumbnail:''} alt="" />              
-                <div className='text-white'>{book.volumeInfo.title}</div>
-              </div>
+            return(book.volumeInfo.imageLinks ? 
+              <div key={book.id} className='min-w-36 max-h-[200px] m-5 relative' >
+                <img className='w-full h-full' src={book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.smallThumbnail:''} alt="immagine copertina" />
+                <div className='absolute inset-x-0 inset-y-0 hidden hover:block'>
+                  <div className='text-white'>Titolo: {book.volumeInfo.title}</div>
+                  <div className='text-white'>Autore: {book.volumeInfo.authors}</div>
+                </div>              
+              </div> : ''
+            )
+          })}
+        </div>
+        {/* II sezione */}
+        <div className='flex overflow-x-auto flex-nowrap'>
+          {datiDue.map((book)=>{
+            return(book.volumeInfo.imageLinks ? 
+              <div key={book.id} className='min-w-36 max-h-[200px] m-5 relative' >
+                <img className='w-full h-full' src={book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.smallThumbnail:''} alt="immagine copertina" />
+                <div className='absolute inset-x-0 inset-y-0 hidden hover:block'>
+                  <div className='text-white'>Titolo: {book.volumeInfo.title}</div>
+                  <div className='text-white'>Autore: {book.volumeInfo.authors}</div>
+                </div>              
+              </div> : ''
             )
           })}
         </div>
