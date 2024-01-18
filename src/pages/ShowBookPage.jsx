14fb5e58c0c5081e639d3fc1d1d867f4d02933f6
SalvatoreBrancato <salvatorebrancato90@gmail.com>
2024-01-18 11:18:47 +0100

    bug fix asincronicità show

--- src/pages/ShowBookPage.jsx
+++ src/pages/ShowBookPage.jsx
@@ -13,7 +13,7 @@ export default function ShowBookPage(){
           .then(response => {
             // Analizza la risposta
             setBook(response.data.volumeInfo);
-            console.log(book)
+            console.log(response.data.volumeInfo)
           })
           .catch(error => {
             // Gestisci gli errori
@@ -23,25 +23,25 @@ export default function ShowBookPage(){
 
       useEffect(apiBook, [])
 
-    return(
+      return(
         <div className=" flex h-screen w-full bg-gradient-to-b from-slate-800  to-slate-600 pt-36">
             {/* lato sinistro */}
            <div className="h-1/2 w-1/3 flex flex-col justify-center items-center">
                 <div className="w-full h-2/3 flex justify-center items-center">
-                    {/* <img src={book.imageLinks.thumbnail} className="" alt="immagine copertina" /> */}
+                    {book && <img src={book.imageLinks.thumbnail} className="" alt="immagine copertina" />}
                 </div>
                 <div className="w-full h-1/3 flex flex-col items-center">
-                    {/* <div className="text-white">Titolo: {book.title}</div>
-                    <div className="text-white">Autore: {book.authors}</div>
-                    <div className="text-white">Pubblicazione: {book.publishedDate}</div>
-                    <div className="text-white">Pagine: {book.pageCount}</div>
-                    <div className="text-white">Lingua: {book.language}</div>                     */}
+                    {book && <div className="text-white">Titolo: {book.title}</div>}
+                    {book && <div className="text-white">Autore: {book.authors}</div>}
+                    {book && <div className="text-white">Pubblicazione: {book.publishedDate}</div>}
+                    {book && <div className="text-white">Pagine: {book.pageCount}</div>}
+                    {book && <div className="text-white">Lingua: {book.language}</div>}                    
                 </div>
            </div>
-
+    
             {/* lato destro */}
            <div className="w-2/3 flex flex-col items-start pr-40">
-                {/* <span className="text-white">{book.description}</span> */}
+                {book && <span className="text-white">{book.description}</span>}
             </div>   
         </div>
     )
