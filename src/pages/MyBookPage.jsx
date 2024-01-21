da1113e63b91bb2c52a06ad7cc3209f105644a53
SalvatoreBrancato <salvatorebrancato90@gmail.com>
2024-01-21 17:13:37 +0100

    totalBooks-totalPages

--- src/pages/MyBookPage.jsx
+++ src/pages/MyBookPage.jsx
@@ -1,10 +1,16 @@
 import {useState, useEffect} from 'react';
 import axios from 'axios';
-import {Link} from "react-router-dom";
+
 
 export default function MyBookPage(){
 
     const [books, setBooks] = useState([])
+
+    //totale pagine lette
+    const [totalPages, setTotalPage] = useState([])
+    let totalPagesRead = 0
+    //totale libri letti
+    const totalBooks = books.length
 
     function apiBack(){
 
@@ -18,6 +24,14 @@ export default function MyBookPage(){
         })
     }
 
+    //calcola il totale delle pagine dei libri letti
+    function calculatesTotalPage(){
+        books.forEach((booksPage)=>{
+          totalPagesRead += booksPage.pages  
+        })
+        setTotalPage(totalPagesRead)   
+    }
+
     function destroyBook(id){
         axios.delete(`http://localhost:3000/booksread/${id}`)
         .then(response =>{
@@ -27,17 +41,22 @@ export default function MyBookPage(){
             console.log(error)
         })
 
+        //ricarica l pagina dopo aver eliminato l'elemento
         window.location.reload()
     }
 
     useEffect(apiBack, []);
+    useEffect(calculatesTotalPage)
 
 
     return(
-        <div className='h-screen flex flex-wrap bg-gradient-to-b from-slate-800  to-slate-600 pt-36'>
+        <div className='min-h-screen bg-gradient-to-b from-slate-800  to-slate-600 pt-36'>
+            <div className='text-white ms-10'>Libri letti: {totalBooks}</div>
+            <div className='text-white ms-10'>Pagine lette: {totalPages}</div>
+            <div className='flex flex-wrap'>
             {books.map((book)=>{
                 return(
-                    <div key={book.id} className='h-1/3 w-1/6 px-3'>
+                    <div key={book.id} className='h-1/3 w-1/6 px-3 mb-10'>
                         <div className='flex justify-center my-3'>
                             <img src={book.image} alt="immagine copertina" />
                         </div>
@@ -52,6 +71,8 @@ export default function MyBookPage(){
                     </div>
                 )
             })}
+            </div>
+            
         </div>
     )
 }
